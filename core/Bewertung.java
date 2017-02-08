ad88ea81f36c438244a482ba22423f67f29ba66b
Lukas Jung <olkixid@users.noreply.github.com>
2017-02-08 10:51:11 +0100

    new Bewertung

--- core/Bewertung.java
+++ core/Bewertung.java
@@ -1,6 +1,28 @@
 package easyexam.core;
-//TODO:immutable machen
+
 public class Bewertung {
-	public double punkte;
-	public String hinweis;
+	private double punkte;
+	private String hinweis;
+	private boolean valid;
+	
+	public Bewertung(double punkte, String hinweis) {
+		this.punkte = punkte;
+		this.hinweis = hinweis;
+		
+		if (punkte > 0.0) {
+			this.valid = true;
+		}
+	}
+	
+	public double getPunkte() {
+		return punkte;
+	}
+
+	public String getHinweis() {
+		return hinweis;
+	}
+
+	public boolean isValid() {
+		return valid;
+	}
 }
