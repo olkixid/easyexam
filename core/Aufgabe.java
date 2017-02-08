a8d8a48c23b7553e02cfd0eafd647932ea026af1
Lukas Jung <olkixid@users.noreply.github.com>
2017-02-08 10:43:30 +0100

    added valid field to Aufgabe

--- core/Aufgabe.java
+++ core/Aufgabe.java
@@ -1,16 +1,26 @@
 package easyexam.core;
+
 public class Aufgabe {
 	private double maxPunkte;
 	private String beschreibung;
+	private boolean valid;
 	
 	public Aufgabe(double maxPunkte, String beschreibung) {
 		this.maxPunkte = maxPunkte;
 		this.beschreibung = beschreibung;
+		
+		if (maxPunkte > 0.0) {
+			this.valid = true;
+		}
 	}
 	public double getMaxPunkte() {
 		return maxPunkte;
 	}
 	public String getBeschreibung() {
 		return beschreibung;
+	}
+	
+	public boolean isValid() {
+		return valid;
 	}
 }
